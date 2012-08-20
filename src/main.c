74292b5a45eebffe5940f8440a220b7e2e129f93
rensuiyi <ren084773820@gmail.com>
2012-08-20 19:11:45 +0800

    add main file

---
+++ src/main.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  part of tcpserver
+ *
+ *        Version:  1.0
+ *        Created:  2012年08月20日 19时09分14秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+int main()
+{
+	printf("tcp server start\n");
+	return 0;
+}
+
