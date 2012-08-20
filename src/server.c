76ebc4ee9c2f7f9e52b180922c99c17d6927dcf7
rensuiyi <ren084773820@gmail.com>
2012-08-20 21:48:39 +0800

    add server.c server.h

---
+++ src/server.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  server.c
+ *
+ *    Description:  part of server
+ *
+ *        Version:  1.0
+ *        Created:  2012年08月20日 21时45分38秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  rensuiyi 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+
+
