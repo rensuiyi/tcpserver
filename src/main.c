4d082fc3ffe44b38dd07c234b686636ccb9fb293
rensuiyi <ren084773820@gmail.com>
2012-08-21 22:16:52 +0800

    do nothing important

--- src/main.c
+++ src/main.c
@@ -20,9 +20,32 @@
 #include <string.h>
 #include <errno.h>
 
-int main()
+#define SERVER_PORT 5000
+#define MAX_LISTEN  5
+int main(int argc,char *argv[])
 {
 	printf("tcp server start\n");
+	int port;
+	int max_listen;
+	if((argc>1)&&(argv[1]!=NULL))
+	{
+        port=atoi(argv[1]);
+	}
+	else
+	{
+		port=SERVER_PORT;
+	}
+    if((argc==2)&&(argv[2]!=NULL))
+	{
+		max_listen=atoi(argv[2]);
+	}
+	else
+	{
+		max_listen=MAX_LISTEN;
+	}
+   
+   printf("======PORT:%d     MAX LISTEN:%d=====\n",port,max_listen);
 	return 0;
+	
 }
 
